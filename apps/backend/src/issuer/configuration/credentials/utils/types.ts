51fa8d20c75b321e114bd37fbd383ed96bb39254
Mirko Mollik <mirko.mollik@eudi.sprind.org>
2026-05-19 17:00:30 +0200

    fix: address SonarCloud issues and refactor client assets (#729)
    * fix: use local fonts
    
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>
    
    * fix: limit size
    
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>
    
    * fix: work on sonarcube errors
    
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>
    
    * chore: format
    
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>
    
    * fix: migration scripts
    
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>
    
    * chore: format
    
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>
    
    * fix: remove redundant code
    
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>
    
    * fix: license check
    
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>
    
    * fix: add missing dependencies
    
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>
    
    ---------
    
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>

---
+++ apps/backend/src/issuer/configuration/credentials/utils/types.ts
@@ -0,0 +1,51 @@
+type FieldType =
+    | "string"
+    | "number"
+    | "integer"
+    | "boolean"
+    | "object"
+    | "array"
+    | "date";
+
+type ClaimPathElement = string | number | null;
+
+interface FieldDisplay {
+    locale: string;
+    name: string;
+    description?: string;
+}
+
+export interface ClaimFieldDefinition {
+    path: ClaimPathElement[];
+    type: FieldType;
+    defaultValue?: unknown;
+    mandatory?: boolean;
+    disclosable?: boolean;
+    namespace?: string;
+    display?: FieldDisplay[];
+    constraints?: Record<string, unknown>;
+}
+
+/** @internal Used by derive functions to build runtime metadata for issuance. */
+export interface ClaimDisplayInfo {
+    name?: string;
+    locale?: string;
+}
+
+/** @internal Used by derive functions to build runtime metadata for issuance. */
+export interface ClaimMetadata {
+    path: ClaimPathElement[];
+    mandatory?: boolean;
+    display?: ClaimDisplayInfo[];
+}
+
+export interface JsonSchema {
+    $schema?: string;
+    type?: string;
+    title?: string;
+    description?: string;
+    properties?: Record<string, JsonSchema>;
+    required?: string[];
+    items?: JsonSchema;
+    [key: string]: unknown;
+}
